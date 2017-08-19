3ef1aa412453926a3412a7937a575ab68662e97a
To Nguyen Thanh Vu <tntvu@TMA.COM.VN>
2017-08-19 16:39:47 +0700

    nuxtjs oh oh

---
+++ nuxt.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  /*
+  ** Build configuration
+  */
+  build: {},
+  /*
+  ** Headers
+  ** Common headers are already provided by @nuxtjs/pwa preset
+  */
+  head: {},
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Customize app manifest
+  */
+  manifest: {
+    theme_color: '#3B8070'
+  },
+  /*
+  ** Modules
+  */
+  modules: [
+    '@nuxtjs/pwa',
+    '@nuxtjs/proxy'
+  ],
+  proxy: { 
+    '/api/books': 'http://localhost:4000/books'
+  },
+  plugins: [
+    '~/plugins/vue-resource'
+  ]
+}
